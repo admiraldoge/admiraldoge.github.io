202d8904965a6f6049099218a63485da2c554120
Maximilian Vino <max.vinsum@gmail.com>
2021-03-10 22:00:15 -0400

    Orbit to fit new model

--- components/PicassoAnimationContainer.tsx
+++ components/PicassoAnimationContainer.tsx
@@ -9,38 +9,29 @@ import OrbitalAnimation from "./OrbitalAnimation";
 const PicassoAnimationContainer: React.FunctionComponent = ({}) => (
     <div className={styles.ctn}>
         <OrbitalAnimation
-            idx={2}
-            radius={250}
-            figureSize={80}
-            translateX={-250}
-            translateY={-250}
+            idx={3}
+            radius={50}
+            figureSize={20}
+            translateX={1000}
+            translateY={200}
             orbitProps={{color:"#05244D", width:2}}
             figureProps={{type: "circle", color:"#a02921"}}
         />
         <OrbitalAnimation
-            idx={2}
-            radius={300}
-            figureSize={80}
-            translateX={100}
+            idx={3}
+            radius={100}
+            figureSize={20}
+            translateX={900}
             translateY={140}
             orbitProps={{color:"#05244D", width:2}}
             figureProps={{type: "circle", color:"#a02921"}}
         />
         <OrbitalAnimation
-            idx={2}
-            radius={400}
-            figureSize={80}
-            translateX={600}
-            translateY={-400}
-            orbitProps={{color:"#05244D", width:2}}
-            figureProps={{type: "circle", color:"#a02921"}}
-        />
-        <OrbitalAnimation
-            idx={1}
+            idx={3}
             radius={150}
-            figureSize={80}
-            translateX={700}
-            translateY={450}
+            figureSize={20}
+            translateX={800}
+            translateY={120}
             orbitProps={{color:"#05244D", width:2}}
             figureProps={{type: "triangle", color:"#a02921"}}
         />
